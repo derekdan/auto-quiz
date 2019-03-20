884c4cbddd4a62e18156e9fc6a68f462df2bdd2a
Andy <yumilj@126.com>
2019-03-20 19:30:07 +0800

    Add files via upload

---
+++ autorun.sh
@@ -0,0 +1,113 @@
+#! /bin/sh
+
+int=0
+while(( $int<1 ))
+do
+	python test.py "http://api.yiqiapp.cn/dkdtweb/?user_name=13802880192&pd=755466dfb30e23f60b1d79f62bca21c1&type=pc&sign=26f91c18da69c2b8b061f7090c0e38e6&mobile=0"  #jike
+	let "int++"
+done
+
+int=0
+while(( $int<1 ))
+do
+	python test.py "http://api.yiqiapp.cn/dkdtweb/?user_name=13802885071&pd=4570f5f0388498920b1d79f62bca21c1&type=pc&sign=3c1b3f9d26e8b64952e2defe926b05f2&mobile=0" #mine
+	let "int++"
+done
+
+int=0
+while(( $int<1 ))
+do
+	python test.py "http://api.yiqiapp.cn/dkdtweb/?user_name=13802882635&pd=f2daf05d1c4e9a5237fa819ad56cf9e1&type=pc&sign=542d888fdb5b027041e4931d5ff644cc&mobile=0" #zhouchangai
+	let "int++"
+done
+
+int=0
+while(( $int<1 ))
+do
+	python test.py "http://api.yiqiapp.cn/dkdtweb/?user_name=13902580510&pd=ee0762fc9b9d573fd887fe82c7ac625d&type=pc&sign=def3b8dfe9f16b1b1cc01fea2d2517ae&mobile=0" #wuyanfang
+	let "int++"
+done
+
+int=0
+while(( $int<1 ))
+do
+	python test.py "http://api.yiqiapp.cn/dkdtweb/?user_name=13802885209&pd=a5d1205eb4698e348f3748be54884523&type=pc&sign=dba2cbc71b2e11917006e0c9ee760152&mobile=0"  #wanglin
+	let "int++"
+done
+
+int=0
+while(( $int<1 ))
+do
+	python test.py "http://api.yiqiapp.cn/dkdtweb/?user_name=13802882136&pd=57eab8d77d650518&type=pc&sign=fb699957fc052f3d3c6db7b5096f16ae&mobile=0"  #weimianyu
+	let "int++"
+done
+
+int=0
+while(( $int<1 ))
+do
+	python test.py "http://api.yiqiapp.cn/dkdtweb/?user_name=13802885373&pd=5cd7a50e1ae87403f0d9842ddce0c5d6&type=pc&sign=892bf26bc1341bf6478d20589c89b0b7&mobile=0"  #yuanmanliu
+	let "int++"
+done
+
+int=0
+while(( $int<1 ))
+do
+	python test.py "http://api.yiqiapp.cn/dkdtweb/?user_name=13802885484&pd=6c766e41d29ddabbc60b0b0295a675ce&type=pc&sign=79422971a7d3710fb1b850d2f20bce87&mobile=0"  #huangjieke
+	let "int++"
+done
+
+int=0
+while(( $int<1 ))
+do
+	python test.py "http://api.yiqiapp.cn/dkdtweb/?user_name=13802881727&pd=f5e904d409666c9e0b1d79f62bca21c1&type=pc&sign=57caaaf8fa9920ca5d6510b442fc47c0&mobile=0"  #yiming
+	let "int++"
+done
+
+int=0
+while(( $int<1 ))
+do
+	python test.py "http://api.yiqiapp.cn/dkdtweb/?user_name=13802882191&pd=908c633090fe05734850380e3d0915e0&type=pc&sign=b91e76041750931553b4ed856a5537d0&mobile=0"  #chenchen
+	let "int++"
+done
+
+int=0
+while(( $int<1 ))
+do
+	python test.py "http://api.yiqiapp.cn/dkdtweb/?user_name=13902220756&pd=a0ff6a548b7d96437b00733e8c51614d&type=pc&sign=2e8b446f3eaf7d96dbbe2ccf772b193f&mobile=0"  #wanggang
+	let "int++"
+done
+
+int=0
+while(( $int<1 ))
+do
+	python test.py "http://api.yiqiapp.cn/dkdtweb/?user_name=13802885335&pd=f7f61e25fc7060c784e089018ad9e348&type=pc&sign=76578c04139356b02ecd17300cb0dcb0&mobile=0"  #heyu
+	let "int++"
+done
+
+int=0
+while(( $int<1 ))
+do
+	python test.py "http://api.yiqiapp.cn/dkdtweb/?user_name=13802885368&pd=96815910eb415a090b1d79f62bca21c1&type=pc&sign=2f3450e110e20c20650b44fe0e97d399&mobile=0"  #huangliangyi
+	let "int++"
+done
+
+int=0
+while(( $int<1 ))
+do
+	python test.py "http://api.yiqiapp.cn/dkdtweb/?user_name=13802880533&pd=70ccb098cb566532&type=pc&sign=e299f3a7384cacbca6b57f1ad533b61f&mobile=0"  #chengchunyan
+	let "int++"
+done
+
+int=0
+while(( $int<1 ))
+do
+	python test.py "http://api.yiqiapp.cn/dkdtweb/?user_name=13802885411&pd=cfe7878afc29e791c65cb6ed42aace77&type=pc&sign=f345ede038c7000cbb35113d02d29300&mobile=0"  #yangwenli
+	let "int++"
+done
+
+int=0
+while(( $int<30 ))
+do
+	python test.py "http://api.yiqiapp.cn/dkdtweb/?user_name=13802885363&pd=f2ad5ee4de1f436e0b1d79f62bca21c1&type=pc&sign=cebb2dcf8434ee006455061d45efeda3&mobile=0"   #heguohuan
+	let "int++"
+done
